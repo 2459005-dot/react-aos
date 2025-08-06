a7d99a3e1ba500f9f3ea985bb57787fa1ac73dfc
2459005-dot <2459005@kbu.ac.kr>
2025-08-06 15:00:24 +0900

    .

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import Hero from './components/Hero'
+import Skill from './components/Skill'
+import Contact from './components/Contact'
+import Aboutme from './components/Aboutme'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+      <div>
+        <Hero/>
+        <Aboutme/>
+        <Contact/>
+        <Skill/>
+      </div>
+  )
+}
+
+export default App
---
+++ src/components/Hero.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <section>
+        <div className='t-wrap'>
+            <h2>Hero title</h2>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Mollitia sint, earum</p>
+        </div>
+    </section>
+)
+}
+
+export default Hero
