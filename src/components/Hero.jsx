a0a0cb0f22f3130068be369c81449c2600294319
2459005-dot <2459005@kbu.ac.kr>
2025-08-07 14:48:29 +0900

    .

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,17 +1,29 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './style/Hero.css'
+import AOS from 'aos'
+import 'aos/dist/aos.css';
 
 const Hero = () => {
-  return (
-    <section className='Hero'>
-        <div className='t-wrap'>
-            <h2>Hero title</h2>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, perspiciatis.
-            </p>
-        </div>
-    </section>
-)
+
+    useEffect(() => {
+        AOS.init({
+            duration: 1000,
+            offset: 120
+        })
+    })
+    return (
+        <section className='Hero'>
+            <div className='t-wrap'>
+                <h2 data-aos='fade-up'>Hero title</h2>
+                <p data-aos='fade-up' data-aos-delay="100">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, perspiciatis.
+                </p>
+
+
+
+            </div>
+        </section>
+    )
 }
 
 export default Hero
